76e9b8b7b4da30238efced824c749cf233af956c
kevinbece5 <kevinbece5@gmail.com>
2019-02-06 12:31:44 -0800

    added styles for comment component

---
+++ client/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+
+export const CommentContainer = styled.div`
+    display: flex;
+    padding: 10px;
+`
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Comment = styled.span`
+    color: #333;
+    padding-left: 10px;
+`;
+
+export const TimeStamp = styled.span`
+    padding-top: 5px;
+    color: #999;
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+    align-items: flex-end;
+    font-size: 12px;
+`;
